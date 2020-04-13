d4c9b4bf94da859877fa3d946aba6d7e5073901e
akshay-srivastav <akshay@somish.com>
2020-04-13 08:48:12 +0530

    mainnet gas and ropsten network

--- truffle-config.js
+++ truffle-config.js
@@ -124,7 +124,7 @@ module.exports = {
                 true // shareNonce
             ),
             network_id: 1, // mainnet's id
-            //gas: 7017622, //
+            gas: 8000000, // max gaslimit
             gasPrice: +process.env.MAINNET_GAS_PRICE || 1000*1000*1000, // default 1 gwei
             //confirmations: 2, // # of confs to wait between deployments. (default: 0)
             timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
@@ -142,14 +142,20 @@ module.exports = {
 
         // Useful for deploying to a public network.
         // NB: It's important to wrap the provider as a function.
-        // ropsten: {
-        // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-        // network_id: 3,       // Ropsten's id
-        // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-        // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-        // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-        // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-        // },
+        ropsten: {
+            provider: () => new HDWalletProvider(
+                process.env.ROPSTEN_MNEMONIC,
+                process.env.ROPSTEN_PROVIDER_URL,
+                0, //address_index
+                10, // num_addresses
+                true // shareNonce
+            ),
+            network_id: 3, // Ropsten's id
+            //gas: 7017622, //
+            //confirmations: 2, // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
+        },
 
     // Useful for private networks
     // private: {
